de1b67b990c7c8520ff9678e9e019a073a10eabb
Em Grasmeder <egrasmed@thoughtworks.com>
2020-02-25 21:03:06 +0100

    Add 'web handler' function

--- src/crossroads/core.clj
+++ src/crossroads/core.clj
@@ -1,10 +1,16 @@
 (ns crossroads.core
-  (:gen-class))
+  (:gen-class)
+  (:require [ring.util.response :as ring-resp]))
 
 (defn write-a-record! [filename record]
   (spit filename record :append true))
+
+(defn pretend-i-got-a-POST-request [filename record]
+  (write-a-record! filename record)
+  (ring-resp/response "ok"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
--- test/crossroads/core_test.clj
+++ test/crossroads/core_test.clj
@@ -1,20 +1,30 @@
 (ns crossroads.core-test
   (:require [clojure.test :refer :all]
             [crossroads.core :as crossroads]
-            [clojure.java.shell :as sh]))
+            [clojure.java.shell :as sh]
+            [ring.util.response :as ring-resp]))
 
+
+(def side-effects-folder "side-effects")
+(def side-effect-file-1 "test-file.txt")
+(def full-side-effect-file-path (format "%s/%s" side-effects-folder side-effect-file-1))
 
 (defn setup []
-  (sh/sh "mkdir" "side-effects"))
+  (sh/sh "mkdir" side-effects-folder))
 
 (defn cleanup []
-  (sh/sh "rm" "-rf" "side-effects"))
+  (sh/sh "rm" "-rf" side-effects-folder))
 
 (use-fixtures :each (fn [f] (setup) (f) (cleanup)))
 
 
-
 (deftest write-a-record-test
   (testing "should do a side effect"
-    (crossroads/write-a-record! "side-effects/test-file.txt" "1 abc")
-    (is (= "1 abc" (:out (sh/sh "cat" "side-effects/test-file.txt"))))))
+    (crossroads/write-a-record! full-side-effect-file-path "1 abc")
+    (is (= "1 abc"
+           (:out (sh/sh "cat" full-side-effect-file-path))))))
+
+(deftest pretend-i-got-a-POST-request-test
+  (testing "should return a happy response"
+    (is (= (ring-resp/response "ok")
+           (crossroads/pretend-i-got-a-POST-request full-side-effect-file-path "doesn't matter")))))
